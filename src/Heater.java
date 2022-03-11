b25b99017905eabb0cafc7eaee0ef664c4789b80
rwieners <rene.wieners@gmail.com>
2022-03-11 20:58:35 +0100

    pretty print und redesigns

--- src/Heater.java
+++ src/Heater.java
@@ -3,28 +3,23 @@ import java.util.*;
 
 public class Heater {
 
-    private int level = 0;
+    private int level;
 
-    public int getLevel(){
-        return this.level;
-    }
-
-    public void setLevel(int level){
+    public void setLevel(int level) {
         this.level = level;
         System.out.println("Heizung auf Stufe: " + level + " eingestellt.");
     }
 
-    public void selectLevel() throws RuntimeException, IOException{
-        int newLevel=0;
+    public void selectLevel() throws RuntimeException, IOException {
+        int newLevel = 0;
 
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Bitte geben Sie eine neue Heizstufe ein (0-5): ");
+            newLevel = scanner.nextInt();
+        }
 
-        System.out.println("Bitte geben Sie eine neue Heizstufe ein (1-5): ");
-        newLevel = scanner.nextInt();
-
-        if(newLevel>5) throw new RuntimeException("Wert ungültig. Es sind nur Werte zwischen 0 und 5 zugelassen");
+        if (newLevel > 5)
+            throw new RuntimeException("Wert ungültig. Es sind nur Werte zwischen 0 und 5 zugelassen");
         this.setLevel(newLevel);
-
     }
-    
 }
